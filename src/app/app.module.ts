3cd8f6dfd5a1ed28bc3d168498e6541b8d9c7b16
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-07-18 16:56:44 +0200

    added things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dish.spec.ts
@@ -0,0 +1,7 @@
+import { Dish } from './shared/dish';
+
+describe('Dish', () => {
+  it('should create an instance', () => {
+    expect(new Dish()).toBeTruthy();
+  });
+});
