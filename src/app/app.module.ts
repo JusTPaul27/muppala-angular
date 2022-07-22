1fc5c71d177d73e2f83c9493122d045f98261a24
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-07-22 11:12:45 +0200

    added things, site doesn't work :-1:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
